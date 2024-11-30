329efa66c66a30ca50f6b95876848f8fd782a8b2
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2024-12-01 00:31:40 +0200

    Add a delete button

--- app/issues/[id]/EditIssueButton.tsx
+++ app/issues/[id]/EditIssueButton.tsx
@@ -2,11 +2,11 @@ import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 import { FaPenToSquare } from "react-icons/fa6";
 
-export default function EditIssueButton({id}:{id:string}) {
+export default function EditIssueButton({issueId}:{issueId:number}) {
   return (
     <Button>
         <FaPenToSquare />
-        <Link href={`/issues/${id}/edit`}>Edit Issue</Link>
+        <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
     </Button>
   )
 }
