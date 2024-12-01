3d6f9ea4be69f6e9c2c801a324e1865a44b0ed3d
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2024-12-01 11:01:05 +0200

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -3,22 +3,29 @@ import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
+type Params = {
+    params: Promise<{id: string}>
+}
 type Body = z.infer<typeof issueSchema>
 export async function PATCH(
     request:NextRequest,
-    {params}:{params: Promise<{id: string}>}){
+    {params}: Params){
+        
     const {id} = await params;
     const body: Body = await request.json();
     const validation = issueSchema.safeParse(body);
+
     if(!validation.success) {
         return NextResponse.json(validation.error.format(), {status: 400});
     };
+
     const issue = await prisma.issue.findUnique({
         where: {id: parseInt(id)}
     });
-    if(!issue){
+
+    if(!issue)
         return NextResponse.json({error: 'Invalid issue'}, {status: 404});
-    };
+    
     const updatedIssue = await prisma.issue.update({
         where: {id: issue.id},
         data: {
@@ -28,4 +35,23 @@ export async function PATCH(
      });
      return NextResponse.json(updatedIssue); 
 
-}
+};
+
+export async function DELETE(
+    request: NextRequest,
+    {params}:Params){
+
+    const {id} = await params;
+    const issue = await prisma.issue.findUnique({
+        where: {id: parseInt(id)}
+    });
+
+    if(!issue)
+        return NextResponse.json({error: 'Invalid issue'}, {status: 404});
+
+    await prisma.issue.delete({
+        where: {id: issue.id}
+    });
+
+    return NextResponse.json({});
+};
