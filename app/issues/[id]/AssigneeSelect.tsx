2ccd4f327a1ce020973b71dde9d48599da94de56
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2024-12-05 00:43:19 +0200

    Populate the asignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,16 +1,36 @@
 'use client'
+import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
 export default function AssigneeSelect() {
+  const [users, setUsers] = useState<User[]>([]);
+  
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const {data} = await axios.get<User[]>('/api/users');
+        setUsers(data)
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      };
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
         <Select.Trigger placeholder='Asign...'/>
-        <Select.Group>
             <Select.Content>
-                <Select.Label>Suggestions</Select.Label>
-                <Select.Item value='1'>Mahmoud Adel</Select.Item>
+                <Select.Group>
+                    <Select.Label>Suggestions</Select.Label>
+                    {users.map(user => (
+                      <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
+                    )}
+                </Select.Group>
             </Select.Content>
-        </Select.Group>
     </Select.Root>
   )
 }
