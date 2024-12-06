6cbb74d4dfc2596b81c6bce1fa7a0b720308adb4
MahmoudAdelI <mahmoud.adeli7888@gmail.com>
2024-12-06 16:45:29 +0200

    Assign an issue to a user

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,36 +1,37 @@
-import prisma from '@/prisma/client';
-import { Box, Flex, Grid } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
-import EditIssueButton from './EditIssueButton';
-import IssueDetails from './IssueDetails';
-import DeleteIssueButton from './DeleteIssueButton';
-import { getServerSession } from 'next-auth';
-import authOptions from '@/app/auth/authOptions';
-import AssigneeSelect from './AssigneeSelect';
+import prisma from "@/prisma/client";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
+import AssigneeSelect from "./AssigneeSelect";
 type Props = {
-    params: Promise<{id: string}>
+  params: Promise<{ id: string }>;
 };
 
-export default async function IssueDetailPage({ params }:Props) {
-    const {id} = await params;
-    const session = await getServerSession(authOptions);
-    const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(id)}
-    });
-    if(!issue) notFound();
+export default async function IssueDetailPage({ params }: Props) {
+  const { id } = await params;
+  const session = await getServerSession(authOptions);
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+  if (!issue) notFound();
   return (
-    <Grid columns={{initial: "1", sm: "5"}} gap="5">
-      <Box className='md:col-span-4'>
-          <IssueDetails issue={issue} />
+    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+      <Box className="md:col-span-4">
+        <IssueDetails issue={issue} />
       </Box>
-      {session &&
-       <Box>
-        <Flex direction="column" gap="4">
-          <AssigneeSelect />
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>}
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
-  )
+  );
 }
